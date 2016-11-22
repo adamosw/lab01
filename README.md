ec23a8f868cf725a55d87a947e2b9f7c2c526fc4
adamosw <adamolejnik@poczta.fm>
2016-11-22 21:33:09 +0100

    Laboratorium 5 - ReverseMe

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-https://rozwal.to/profile/adamosw
+Opis projektu
+
+Generator kluczy dla cwiczenia z plikiem reverse_me.
+
+Wymagania systemowe
+
+    Komputer PC z systemem operacyjnym Windows,
+    Zainstalowany .NET Framework w wersji min. 4.5.2.
+    Opcjonalnie IDE Visual Studio.
+
+Sposób uruchomienia
+
+    W plikach Ÿród³owych pod œcie¿k¹ bin/debug znajduj¹ siê gotowe pliku uruchamialne.
+
+Sposób kompilacji
+
+    Projekty mo¿na uruchomiæ otwieraj¹c pliki .sln w IDE Visual Studio,
+    W celu kompilacji nale¿y z zak³adki Build wybraæ opcjê Rebuild.
+
+Korzystanie z aplikacji 
+    Po uruchomieniu nalezy wprowadzic login. Nastepnie wygenerowane zostanie dla tego loginu haslo.
---
+++ bsiui_keygen/bsiui_keygen/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bsiui_keygen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("USER: ");
+            string login = Console.ReadLine();
+            int number;
+
+            bool isSucceeded = int.TryParse(login, out number);
+
+            if (isSucceeded)
+            {
+                int i = 0;
+                int j;
+                int k = 27;
+                int modulo;
+                int tmp;
+                List<char> result = new List<char>();
+                List<char> alphabet = "abcdefghijklmnopqrstuvwxyz0".ToCharArray().ToList();
+                
+                while (i < 64 && i <= 25)
+                {
+                    j = i + (i * 8);
+                    j = j + number;
+                    tmp = j;
+                    j = j / k;
+                    modulo = tmp % k;
+                    result.Add(alphabet[modulo]);
+
+                    i = i + 1;
+                }
+
+                i = 26;
+                k = 27;
+
+                while (i < 64 && i <= 51)
+                {
+                    j = i * 7;
+                    j = j + number;
+                    tmp = j;
+                    j = j / k;
+                    modulo = tmp % k;
+                    result.Add(alphabet[modulo]);
+
+                    i = i + 1;
+                }
+
+                Console.WriteLine("KEY: {0}", new string(result.ToArray()));
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Invalid number.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
